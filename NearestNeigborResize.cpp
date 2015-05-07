6b3d3d96f1afc37b85ce19e9541ae671b0ef316b
Chris Bubel <christopher.s.bubel@gmail.com>
2015-05-07 16:05:42 -0400

    Updated program

--- NearestNeigborResize.cpp
+++ NearestNeigborResize.cpp
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <iostream>
 #include <cstring>
+#include <time.h>
+#include <sys/time.h>
 
 using namespace std;
 
@@ -23,22 +25,37 @@ NearestNeigborResize::~NearestNeigborResize() {
 	// TODO Auto-generated destructor stub
 }
 
-int * resizePixels(int * pixels, int w1, int h1, int w2, int h2) {
-	int * temp = new int[w2 * h2];
+struct pixel {
+	int r, g, b, a;
+};
+
+/*
+* pixels = array
+* w1 = original width
+* h1 = original height
+* w2 = new width
+* h2 = new height
+*/
+pixel * resizePixels(pixel * pixels, int w1, int h1, int w2, int h2) {
+	pixel * temp = new pixel[w2 * h2];
 	double x_ratio = w1/(double)w2;
 	double y_ratio = h1/(double)h2;
 	double px, py;
 	for (int i=0;i<h2;i++) {
 	        for (int j=0;j<w2;j++) {
-	            px = floor(j*x_ratio) ;
-	            py = floor(i*y_ratio) ;
-	            temp[(i*w2)+j] = pixels[(int)((py*w1)+px)] ;
-	        }
-	    }
-	    return temp;
+	    		px = floor(j*x_ratio);
+	    		py = floor(i*y_ratio);
+	    		temp[(i*w2)+j] = pixels[(int)((py*w1)+px)];
+		}
+	}
+	return temp;
 }
 
 int main() {
+	clock_t t;
+	t = clock();
 	cout << "Hello World!" << endl;
+	t = clock() - t;
+	cout << t << endl;
 	return 0;
 }
